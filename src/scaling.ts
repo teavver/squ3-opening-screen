1bfc6a44aa4b5a8e1d1ed0358ea8109a6ea9b9e8
Jerzy Rutkowski <jerzyx11@gmail.com>
2022-05-19 01:09:25 +0200

    nft_img almost perfect (page2 close to finish)

--- src/scaling.ts
+++ src/scaling.ts
@@ -13,12 +13,31 @@ export default function scalingOnScroll(){
         });
       });
     
+
     // scroll scale function for play button
     const play_btn: HTMLElement | null = document.getElementById('play-btn')
+    const nft_1: HTMLElement | null = document.getElementById('nft-image-1')
+    const nft_2: HTMLElement | null = document.getElementById('nft-image-2')
+    const nft_3: HTMLElement | null = document.getElementById('nft-image-3')
     
     window.onscroll = (event) => {
         const x = window.scrollY / 1000;
         play_btn!.style.transform = `scale(${0.8 - (x/1.5)})`;
+
+        nft_1!.style.transform = `scale(${0.2 + x/2})`;
+        console.log(window.scrollY)
+        nft_2!.style.transform = `scale(${0.2 + x/2})`;
+        console.log(window.scrollY)
+        nft_3!.style.transform = `scale(${0.2 + x/2})`;
+        console.log(window.scrollY)
       };
+
+
+    //   const nft1: HTMLElement | null = document.getElementById('nft-image-1')
+    
+    // window.onscroll = (event) => {
+    //     const nftscroll1 = window.scrollY / 1000;
+    //     nft1!.style.transform = `scale(${0.8 + (nftscroll1/1.5)})`;
+    //   };
 }
 
